8f6f50f301734610103801b4e0456550559d2c24
Lili Rodriguez <lilimrod@gmail.com>
2020-02-14 06:30:24 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,30 +1,31 @@
-function forLoop(array){
-  for (let i = 0; i < 25; i++){
-    if (i===1) {
-      array.push('I am 1 strange loop.')
-    }
-    else {
-      array.push(`I am ${i} strange loop.`)
+function forLoop (array){
+  for(let i = 0;i< 25; i++){
+    if(i === 1){
+      array.push('I am 1 strange loop')
+    }else{
+      array.push(`I am ${i} strange loops`)
     }
   }
   return array
 }
 
-function whileLoop(n){
-  let countdown = n
-  while (countdown > 0){
-    console.log(--countdown)
+function whileLoop (n){
+  let countown = n
+  while (countown > 0){
+    console.log(--countown)
   }
   return 'done'
 }
 
-function doWhileLoop(array){
-  function maybeTrue() {
-  return Math.random() >= 0.5
-  }
-  do {
-    array.pop();
-  }
-  while(array.length > 0 && maybeTrue())
-  return array
+function doWhileLoop(num){
+  var i = 0
+  function incrementVariable() {
+  i ++;
+  return i;
 }
+ 
+do {
+  console.log("I run once regardless");
+} while (incrementVariable() < num);
+}
+
