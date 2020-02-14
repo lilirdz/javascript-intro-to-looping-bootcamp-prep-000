a9669eaa1c484dc323f45c6c418d5bc7ff97f4ed
Lili Rodriguez <lilimrod@gmail.com>
2020-02-14 06:30:42 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -29,3 +29,13 @@ do {
 } while (incrementVariable() < num);
 }
 
+function doWhileLoop(array){
+  function maybeTrue() {
+  return Math.random() >= 0.5
+  }
+  do {
+    array.pop();
+  }
+  while(array.length > 0 && maybeTrue())
+  return array
+}
